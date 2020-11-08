e5736fb7a594a347840b88c5fb65410f9a5f4cfc
Richard Helton <RHelton@acams.org>
2020-11-07 19:22:55 -0700

    initial

---
+++ src/main/java/com/example/demo/controllers/CartController.java
@@ -0,0 +1,80 @@
+package com.example.demo.controllers;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.Item;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.ItemRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.requests.ModifyCartRequest;
+
+@RestController
+@RequestMapping("/api/cart")
+public class CartController {
+
+	public static final Logger log = LoggerFactory.getLogger(CartController.class);
+
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private CartRepository cartRepository;
+	
+	@Autowired
+	private ItemRepository itemRepository;
+	
+	@PostMapping("/addToCart")
+	public ResponseEntity<Cart> addTocart(@RequestBody ModifyCartRequest request) {
+		log.info("Adding modifications to cart for user {}", request.getUsername());
+		User user = userRepository.findByUsername(request.getUsername());
+		if(user == null) {
+			log.error("User {} not found. Cannot add modifications.", request.getUsername());
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		Optional<Item> item = itemRepository.findById(request.getItemId());
+		if(!item.isPresent()) {
+			log.error("Item {} not found. Cannot add modifications.", request.getItemId());
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		Cart cart = user.getCart();
+		IntStream.range(0, request.getQuantity())
+			.forEach(i -> cart.addItem(item.get()));
+		cartRepository.save(cart);
+		log.info("Modifications added to cart successfully for user {}", request.getUsername());
+		return ResponseEntity.ok(cart);
+	}
+	
+	@PostMapping("/removeFromCart")
+	public ResponseEntity<Cart> removeFromcart(@RequestBody ModifyCartRequest request) {
+		log.info("Removing modifications from cart for user {}", request.getUsername());
+		User user = userRepository.findByUsername(request.getUsername());
+		if(user == null) {
+			log.error("User {} not found. Cannot remove modifications.", request.getUsername());
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		Optional<Item> item = itemRepository.findById(request.getItemId());
+		if(!item.isPresent()) {
+			log.error("Item {} not found. Cannot remove modifications.", request.getItemId());
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+		Cart cart = user.getCart();
+		IntStream.range(0, request.getQuantity())
+			.forEach(i -> cart.removeItem(item.get()));
+		cartRepository.save(cart);
+		log.info("Modifications removed from cart successfully for user {}", request.getUsername());
+		return ResponseEntity.ok(cart);
+	}
+		
+}
---
+++ src/main/java/com/example/demo/controllers/OrderController.java
@@ -0,0 +1,51 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.UserOrder;
+import com.example.demo.model.persistence.repositories.OrderRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+	public static final Logger log = LoggerFactory.getLogger(OrderController.class);
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	
+	@PostMapping("/submit/{username}")
+	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
+		log.info("Submitting order for user {}", username);
+		User user = userRepository.findByUsername(username);
+		if(user == null) {
+			log.error("User {} not found. Cannot submit order.", username);
+			return ResponseEntity.notFound().build();
+		}
+		UserOrder order = UserOrder.createFromCart(user.getCart());
+		orderRepository.save(order);
+		log.info("Order for user {} submitted successfully.", username);
+		return ResponseEntity.ok(order);
+	}
+	
+	@GetMapping("/history/{username}")
+	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
+		log.info("Getting order history for user {}", username);
+		User user = userRepository.findByUsername(username);
+		if(user == null) {
+			log.error("User {} not found. Cannot submit order.", username);
+			return ResponseEntity.notFound().build();
+		}
+		return ResponseEntity.ok(orderRepository.findByUser(user));
+	}
+}
